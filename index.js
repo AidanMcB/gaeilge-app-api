e48ea1facfbff99521bc6e8f73a4e07b39c62bf7
Aidan McBride <aidankmcbride@gmail.com>
2025-10-12 16:35:42 -0400

    do not whitelist localhost

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const corsOptions = {
     origin: [
         process.env.FRONTEND_ORIGIN,
         'https://gaeilge-app.vercel.app',
-        'http://localhost:5173',
-        'http://localhost:3000'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
